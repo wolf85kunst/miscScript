cb28eb2b5140a29a25d6aea3e53ecc7ce064d629
hugo <hugo.gutekunst@gmail.com>
2020-02-04 23:48:40 +0100

    minor changes

--- compose-running-plan.sh
+++ compose-running-plan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Hugo GUTEKUNST Feb. 2020
+# Hugo GUTEKUNST
+# Feb. 2020
 
 # DESCRIPTION
 # ------------------------------------------------------------------------------------------------------
@@ -8,10 +9,10 @@
 
 # SETINGS
 # ------------------------------------------------------------------------------------------------------
-# Global training setings
+# Global setings
 beginningDate='20200101'	# Date of the first monday of the training plan. Format : "YYYYmmdd"
 lastMondayDate='20200324'	# Date of the last week of the training plan. Format : "YYYYmmdd"
-numberOfWeekForTraining=16	# Number of weeks for the training plan
+numberOfWeekForTraining=20	# Number of weeks for the training plan
 
 declare -a runFrequency		# Running frequency per week (time to go)
 				# Declaration syntax : "Number of runs per week ; Number of weeks ; comment"
@@ -27,23 +28,19 @@ volumeTarget=70			# The maximum running volume desired in a week (KM)
 # Long Run
 firstLongRun=7			# week number of the first long run (week number)
 initialLongRun=10		# The first long run in kilometer (KM)
-longRunTarget=30 		# The longest run desired in the training plan (KM)
+longRunTarget=30 		# The biggest desired run in the training plan (KM)
 
-# Rest
-sharpening=2			# Number of weeks before competition for sharpening
+# Volume reduction (sharpening)
 longRunBefore=4			# Number of weeks after the longuest run
-
-# Volume reduction
-#scaleBackP1=50			# The first phase of volume reduction (percentage)
-#scaleBackP2=10			# The second phase of volume reduction (percentage)
-
-declare -a sharpening		
+declare -a sharpening		# Sharpening period. Declare as many lines as there will be sharpening week after ${volumeTarget}		
+				# Declaration syntax : "percent of volume ; Run per week"
 sharpening[0]='50;3'
 sharpening[1]='10;1'
+sharpening[2]='5;1'
 
 # Misc settings
 scaleNumber=0			# Precision of float number on display
-tempFile=/tmp/plan.txt
+tempFile=/tmp/plan.txt		# Temporary file to draw the array
 
 # Colors definition		# Bash color code library
 colorRed="\e[31m"
@@ -56,18 +53,28 @@ colorBackgroundBlue="\e[44m"
 # ------------------------------------------------------------------------------------------------------
 printLine(){
 	char='='
-	repetition=40
+	repetition=75
 	for i in $(seq 1 ${repetition}) ; do echo -n ${char} ; done
 	echo
+}
+testNumber(){
+	regex='^[0-9]+([.][0-9]+)?$'
+	if [[ ${1} =~ $regex ]]; then
+		return 0
+	else
+		return 1
+	fi
 }
 scaleNumber(){
 	float=${1}
 	scale=${2}
-	#if ! [[ ${float} =~ '^[0-9.]+$' ]]; then
-	if [ ${scale} -eq 0 ]; then
-		echo -n ${float} | cut -d'.' -f1
-	else
-		echo -n ${float} | sed -r "s/([0-9]*\.[0-9]{${scale}}).*/\1/"
+	if $(testNumber ${float}); then
+		#echo NOMBRE
+		if [ ${scale} -eq 0 ]; then
+			echo -n ${float} | cut -d'.' -f1
+		else
+			echo -n ${float} | sed -r "s/([0-9]*\.[0-9]{${scale}}).*/\1/"
+		fi
 	fi
 }
 rewriteRunFrequency(){
@@ -143,54 +150,52 @@ calcAvgSingle(){
 		avgSingle="${avg}"
 	fi
 }
+increaseDate(){
+		weekDate=$(date '+%Y%m%d' -d "${1}+7 days")
+}
 printTrainingPlan(){
 	weekNumber=${i}
-	dateFormat="$(date +%d/%m/%Y -d ${2})"
-	weekVolume="$(scaleNumber ${3} ${scaleNumber})"
-	runPerWeek="${4}"
-	avg="$(scaleNumber ${5} 2)"
-	longRun="$(scaleNumber ${6} ${scaleNumber})"
-	avgSingle="$(scaleNumber ${7} 2)"
+	dateFormat="${colorMagenta}$(date +%d/%m/%Y -d ${2})${colorNormal}"
+	weekVolume="${colorGreen}$(scaleNumber ${3} ${scaleNumber})${colorNormal}km"
+	runPerWeek="${colorGreen}${4}${colorNormal}"
+	avg="${colorGreen}$(scaleNumber ${5} 2)${colorNormal}km"
+	if testNumber ${6}; then
+		longRun="${colorGreen}$(scaleNumber ${6} ${scaleNumber})${colorNormal}km"
+	else
+		longRun="${colorRed}${6}${colorNormal}"
+	fi
+	avgSingle="${colorGreen}$(scaleNumber ${7} 2)${colorNormal}km"
 
-	echo -e "S${weekNumber}|[${dateFormat}]|volume=${colorGreen}${weekVolume}${colorNormal}km|run=${runPerWeek}|avg=${avg}km|LR=${longRun}|avgS=${avgSingle}km" >> ${tempFile}
-}
-increaseDate(){
-	weekDate=$(date '+%Y%m%d' -d "${1}+7 days")
+	echo -e "S${weekNumber}|[${dateFormat}]|volume=${weekVolume}|run=${runPerWeek}|avg=${avg}|LR=${longRun}|avgS=${avgSingle}" >> ${tempFile}
 }
 
 # MAIN
 # ------------------------------------------------------------------------------------------------------
+# Some control before starting
 rewriteRunFrequency
 if [ -f ${tempFile} ]; then rm ${tempFile}; fi
+
 longRunPeriod=$((${numberOfWeekForTraining}-${longRunBefore}-${firstLongRun}+1))
 volumePeriod=$((${numberOfWeekForTraining}-${#sharpening[@]}))
-
 interval=$(bc -l <<<"(${volumeTarget}-${initialVolume})/(${volumePeriod}-1)")
 intervalLongRun=$(bc -l <<<"(${longRunTarget}-${initialLongRun})/(${longRunPeriod}-1)")
 
-# ----------------------------------
-# Init var
-# ----------------------------------
-longRunCpt=0
 weekDate=${beginningDate}
-weekVolume=${initialVolume}
-longRun=''
 
 # ----------------------------------
 # SUMMARY
 # ----------------------------------
 echo -e "${colorNormal}"
 printLine
-echo -e "${colorGreen}SUMMARY${colorNormal}"
+echo -e " ${colorGreen}SUMMARY${colorNormal}"
 printLine
-
-echo -e "* Your training plan will start on [${colorMagenta}$(date '+%d/%m/%Y' -d ${beginningDate})${colorNormal}] & stop on [${colorMagenta}$(date '+%d/%m/%Y' -d ${lastMondayDate})${colorNormal}] !"
+echo -e "* Your training plan will start on [${colorMagenta}$(date '+%d/%m/%Y' -d ${beginningDate})${colorNormal}] and stop on [${colorMagenta}$(date '+%d/%m/%Y' -d ${lastMondayDate})${colorNormal}] !"
 echo -e "* Your training plan will last ${colorGreen}${numberOfWeekForTraining}${colorNormal} weeks."
 echo
 echo -e "* Increase period :"
 echo -e "\tYou will increase your volume during ${colorGreen}${volumePeriod}${colorNormal} weeks : \
 ${initialVolume} Km to ${volumeTarget} Km (+ ~${colorGreen}$(scaleNumber ${interval} 2)${colorNormal} Km each week)."
-echo -e "\tYou will increase your long run during ${colorGreen}${longRunPeriod}${colorNormal} weeks : \
+echo -e "\tYou will increase your long run (LR) during ${colorGreen}${longRunPeriod}${colorNormal} weeks : \
 ${initialLongRun} Km to ${longRunTarget} (+ ~${colorGreen}$(scaleNumber ${intervalLongRun} 2)${colorNormal} Km each week)."
 echo
 echo -e "* Different training phase :"
@@ -199,12 +204,13 @@ for i in ${!runFrequency[@]}; do
 	echo -e "\tP$((${i}+1))) ${colorGreen}$(echo ${runFrequency[${i}]} | cut -d';' -f1)${colorNormal} trainings per week during ${colorGreen}$(echo ${runFrequency[${i}]} |cut -d';' -f2)${colorNormal} weeks."
 done
 echo
+echo
 
 # ----------------------------------
 # TRAINING PLAN
 # ----------------------------------
 printLine
-echo -e "${colorGreen}TRAINING PLAN${colorNormal}"
+echo -e " ${colorGreen}TRAINING PLAN${colorNormal}"
 printLine
 
 for i in $(seq 1 ${numberOfWeekForTraining}); do
@@ -219,4 +225,5 @@ for i in $(seq 1 ${numberOfWeekForTraining}); do
 		increaseDate ${weekDate} 
 done
 
+# Draw array
 column ${tempFile} -t -s "|"
